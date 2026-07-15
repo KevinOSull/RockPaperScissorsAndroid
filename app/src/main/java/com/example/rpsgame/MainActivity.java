fc86c4d92c8396e74ba3e4e60fde237e7380f37a
KevinOSull <kosullivan3336@gmail.com>
2026-07-15 02:42:35 +0100

    Added arrays to store buttons

--- app/src/main/java/com/example/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rpsgame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rpsgame;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NO_DRAWABLE = -1;
+    private final int currentDrawable = NO_DRAWABLE;
+    private static final int[] GAME_IMAGES = {
+            R.drawable.paper,
+            R.drawable.scissors,
+            R.drawable.rock
+    };
+
+    private int[] winnerImages = new int[]{};
+    private int[]loserImages = new int[]{};
+
+    private Button rockButton;
+    private Button scissorsButton;
+    private Button paperButton;
+    private Button bestOfThreeGamesBtn;
+    private Button bestOfFiveGamesBtn;
+    private Button bestOfTenGamesBtn;
+    private Button resetGameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void findViews(){
+
     }
 }
