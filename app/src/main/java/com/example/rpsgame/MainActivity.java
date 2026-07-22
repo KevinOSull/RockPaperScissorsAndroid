c5ebfa5bf15b55d7f8833557f44e08a1e067da4e
KevinOSull <kosullivan3336@gmail.com>
2026-07-22 13:40:24 +0100

    Added variables

--- app/src/main/java/com/example/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rpsgame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rpsgame;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,42 +10,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final int NO_DRAWABLE = -1;
-    private final int currentDrawable = NO_DRAWABLE;
-    private static final int[] GAME_IMAGES = {
-            R.drawable.paper,
-            R.drawable.scissors,
-            R.drawable.rock
-    };
+   private static final Random RANDOM_GENERATOR = new Random();
+   private static GameStatus gameStatus;
+   private static final int BEST_OF_THREE_GAMES = 3;
+   private static final int BEST_OF_FIVE_GAMES = 5;
+   private static final int BEST_OF_TEN_GAMES = 10;
 
-    private int[] winnerImages = new int[]{};
-    private int[]loserImages = new int[]{};
-    private static final int BEST_OF_THREE_GAMES = 3;
-    private static final int BEST_OF_FIVE_GAMES = 5;
-    private static final int BEST_OF_TEN_GAMES = 10;
-    private static final int BEST_OF_THREE = 1;
-    private static final int BEST_OF_FIVE = 2;
-    private static final int BEST_OF_TEN = 3;
-    private Button rockBtn;
-    private Button scissorsBtn;
-    private Button paperBtn;
-    private Button bestOfThreeGamesBtn;
-    private Button bestOfFiveGamesBtn;
-    private Button bestOfTenGamesBtn;
-    private Button resetGameBtn;
-    private int level;
-    private int turns;
-    private int buttonId;
-    private int computerScore = 0;
-    private int playerScore = 0;
-    private int playerRoundScore = 0;
-    private int computerRoundScore = 0;
-    private int playerChoice;
-    private int computerChoice;
-    private static GameStatus gameStatus = GameStatus.GAME_IN_PROGRESS;
-    private static int foo;
+   private static final int BEST_OF_THREE = 1;
+   private static final int BEST_OF_FIVE = 2;
+   private static final int BEST_OF_TEN = 3;
+
+   private int[] buttonNumbers = new int[]{BEST_OF_THREE,BEST_OF_FIVE,BEST_OF_TEN};
+   private int[] numberOfRounds = new int[]{BEST_OF_THREE_GAMES,BEST_OF_FIVE_GAMES,BEST_OF_TEN_GAMES};
+   private String[] roundNumber = new String[]{"Best of Three","Best of Five","Best of Ten"};
+
+   private int computerChoice;
+   private int playerChoice;
+   private int playerScore = 0;
+   private int computerScore = 0;
+   private int computerRoundScore = 0;
+   private int playerRoundScore = 0;
+   private int buttonId;
+   private int turns;
+   private int level;
+   private int choice;
+   private int roundRemaining;
+   private String message;
+   private String selectedMode;
+
+   private TextView[] numberOfGamesLabel;
+   private Button[] numberOfGamesButtons;
+   private Button[] gameFlowButtons;
+   private Button[] gameFlowLabels;
 
 
     @Override
