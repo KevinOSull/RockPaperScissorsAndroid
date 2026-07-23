0af0a3e8f17c1f4f9a9a48fa05288fe9e6648a88
KevinOSull <kosullivan3336@gmail.com>
2026-07-23 23:12:09 +0100

    Started filling in method with logic for game flow buttons and number of games Buttons

--- app/src/main/java/com/example/rpsgame/MainActivity.java
+++ app/src/main/java/com/example/rpsgame/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
    private int[] buttonNumbers = new int[]{BEST_OF_THREE,BEST_OF_FIVE,BEST_OF_TEN};
    private int[] numberOfRounds = new int[]{BEST_OF_THREE_GAMES,BEST_OF_FIVE_GAMES,BEST_OF_TEN_GAMES};
-   private String[] roundNumber = new String[]{"Best of Three","Best of Five","Best of Ten"};
+   private String[] roundNumbers = new String[]{"Best of Three","Best of Five","Best of Ten"};
 
    private int[] gameFlowImages = new int[]{R.drawable.rock,R.drawable.paper,R.drawable.scissors};
    private int[] winnerImages = new int[]{R.drawable.trophy,R.drawable.winner2};
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
    private int level;
    private int choice;
    private int roundRemaining;
+   private int targetWins;
    private String message;
    private String selectedMode;
 
@@ -80,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
    private Runnable clearScreenTimer;
    private Runnable displayWhoWonTimer;
+   private Runnable clearLastImage;
 
    private GameStatus gameStatus = GameStatus.GAME_IN_PROGRESS;
    @Override
@@ -127,22 +129,158 @@ public class MainActivity extends AppCompatActivity {
        });
    }
 
-   private void setChoice(View v){
+   private Runnable showTemporaryMessage(TextView view,int duration,Runnable runnable){
+       view.removeCallbacks(runnable);
+       Runnable newTask = new Runnable(){
+           @Override
+           public void run() {
+               view.setText("");
+           }
+       };
+       view.postDelayed(newTask,duration);
+       return newTask;
+   }
 
+   private void showTemporaryImage(ImageView imageView){
+       imageView.removeCallbacks(clearLastImage);
+       clearLastImage = new Runnable(){
+           public void run(){
+               imageView.setVisibility(View.INVISIBLE);
+           }
+       };
+       imageView.postDelayed(clearLastImage,5000);
+   }
+
+   private void setChoice(View v){
+        for(int i = 0; i < gameFlowButtons.length; i++){
+            if(v.getId() == gameFlowButtons[i].getId()){
+                playerChoice = i;
+                printPlayerChoice(gameFlowLabels[i],gameFlowImages[i]);
+            }
+        }
+   }
+   private void printPlayerChoice(TextView view,int imageIcon){
+        view.setText(imageIcon);
+    }
+
+    private int setNumberOfGames(View v){
+        for(int i = 0; i < numberOfGamesButtons.length; i++){
+            if(v.getId() == numberOfGamesButtons[i].getId()){
+                buttonId = buttonNumbers[i];
+                selectedMode = roundNumbers[i];
+            }
+        }
+        buttonControl(numberOfGamesButtons,false);
+        buttonControl(gameFlowButtons,true);
+        turns = processButton();
+        return buttonId;
+    }
+
+    private int processButton(){
+        for(int i = 0; i < buttonNumbers.length; i++){
+            level = buttonNumbers[i];
+            roundRemaining = level;
+            targetWins = calculateTargetWins(level);
+            activeLabel = numberOfGamesLabel[i];
+            printOutNumberOfGames(activeLabel,numberOfRounds[i]);
+        }
+        return buttonId;
+    }
+    private void checkWhoWon(){
+
+   }
+
+   private boolean hasComputerWon(){
+       return true;
+   }
+
+   private boolean hasPlayerWon(){
+       return true;
+   }
+
+   private boolean isGameDrawn(){
+       return true;
+   }
+
+   private String getGameMessage(String message){
+       return "";
+   }
+
+   private int calculateTargetWins(int totalRounds){
+       return 0;
    }
 
    private void resolveRound(){
 
    }
 
-   private int setNumberOfGames(View v){
-       return 0;
+   private void scheduleWinTextClear(){
+
    }
 
-   private int processButton(){
-       return 0;
+   private void printOutWhoWon(String message){
+
    }
-    private void findViews(){
+
+   private void resetGameValues(){
+
+   }
+
+   private void resetGameButtons(){
+
+   }
+
+   private void resetGameScores(){
+
+   }
+
+   private void resetGameImages(){
+
+   }
+
+   private void resetGameLabels(){
+
+   }
+   private void printOutNumberOfGames(TextView view,int numberOfGames){
+
+   }
+
+   private void buttonControl(Button[] buttons,boolean isEnabled){
+
+   }
+
+   private void setComputerImage(){
+
+   }
+
+   private void printOutCurrentRoundScores(TextView view,int roundScore){
+
+   }
+
+   private void printCurrentScores(TextView view,int score){
+
+   }
+
+   private void updateScore(){
+
+   }
+
+   private void checkRoundWinner(){
+
+   }
+
+   private void handleScoringSystem(boolean isPlayer,String winnerName){
+
+   }
+
+   private void checkRoundTurns(int roundScore){
+
+   }
+
+   private void resetHandScores(){
+
+   }
+   private void findViews(){
         submitButton = findViewById(R.id.submit_button);
         resetButton = findViewById(R.id.reset_button);
         rockButton = findViewById(R.id.rock_button);
